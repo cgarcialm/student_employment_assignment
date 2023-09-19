323ade51ef6380878a84c3543ae071bdab3f0186
Cecilia Garcia Lopez de Munain <ceci.garcialm@gmail.com>
2023-09-18 22:49:55 -0700

    Move main method to Demo class

--- src/StudentEmploymentAssignment.java
+++ src/StudentEmploymentAssignment.java
@@ -180,21 +180,4 @@ public class StudentEmploymentAssignment {
 
         return resultStatus;
     }
-
-    /**
-     * Main method to run the Student Employment Assignment problem solver.
-     *
-     * @param args Command-line arguments (not used).
-     */
-    public static void main(final String[] args) {
-        Loader.loadNativeLibraries();
-        final int[][] profPreferences = {{1, 2}, {2, 1}, {1, 1}, {3, 2}};
-        final int[][] studPreferences = {{1, 2}, {2, 1}, {1, 1}, {3, 2}};
-        final int[] hoursPerClass = {10, 10, 10};
-        final StudentEmploymentAssignment problem = new
-                StudentEmploymentAssignment(profPreferences, studPreferences,
-                hoursPerClass);
-        problem.solve();
-        problem.printSolution();
-    }
 }
